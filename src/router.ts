7a819a30e2d815abe65c90fd5f0045af30238f4b
Milton Munoz <livemilton@gmail.com>
2026-01-24 13:00:07 -0500

    express :sparkles: Creating the endpoint to obtain all products

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,13 @@
 
 import { Router } from "express"
-import { createProduct } from "./handlers/product"
+import { createProduct, getProducts } from "./handlers/product"
 import { body } from "express-validator"
 import { handleInputErrors } from "./middleware"
 
 const router = Router()
 
 // Routing
-router.get('/',(req, res) => {
-
-   res.json('Desde GET')
-})
+router.get('/', getProducts)
 
 router.post('/', 
 
