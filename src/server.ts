bb27684c34162337d003bc84d3cc866e52784dcd
Milton Munoz <livemilton@gmail.com>
2026-01-24 08:15:08 -0500

    express :sparkles: Add colors to console messages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './router';
 import db from './config/db'
+import colors from 'colors';
 
 // Conectar a base de datos
 async function connectDB(){
@@ -8,10 +9,10 @@ async function connectDB(){
     try {
         await db.authenticate()
         db.sync()
-        console.log('Conexion exitosa a la BD')
+        console.log(colors.blue('Conexion exitosa a la BD'))
     } catch (error) {
         console.log(error)
-        console.log('Hubo un error al conectar a la BD')
+        console.log(colors.red.bold('Hubo un error al conectar a la BD'))
     }
 }
 connectDB()
