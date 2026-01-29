fa63298f099fb7d11e4249d22880eb8b53816c32
Milton Munoz <livemilton@gmail.com>
2026-01-28 21:13:33 -0500

    Documenting all Products

--- src/router.ts
+++ src/router.ts
@@ -33,6 +33,25 @@ const router = Router()
  *
  */
 
+
+/**
+ * @swagger
+ * /api/products: 
+ *      get:
+ *          summary: Get a list of products
+ *          tags: 
+ *               - Products
+ *          description: Return a list of products
+ *          responses: 
+ *              200:
+ *                  description: Successful response
+ *                  content:
+ *                      application/json:
+ *                              schema:
+ *                                  type: array
+ *                                  items:
+ *                                      $ref: '#/components/schemas/Product'
+ */
 // Routing
 router.get('/', getProducts)
 
